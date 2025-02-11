1fb282324ad5bf62d72235c9e292240e47e5a4ed
jjgresham <jordanjgresham@gmail.com>
2025-02-11 16:17:39 -0700

    Updated distribution ID

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -4,5 +4,5 @@
 aws s3 sync $SOURCE_DIR s3://$AWS_S3_BUCKET/ --region $AWS_REGION --delete
 
 # Invalidate CloudFront cache
-# distribution_id="E36OGDRKNU65XQ"
-# aws cloudfront create-invalidation --distribution-id $distribution_id --paths "/*"
+distribution_id="E3UHWQ4N62WTGX"
+aws cloudfront create-invalidation --distribution-id $distribution_id --paths "/*"
